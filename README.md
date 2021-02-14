8703368b8c5fa4d696018283c36097afd08528d8
Ary Sayd Sault <sault.sayd.ary@gmail.com>
2021-02-14 18:29:17 -0300

    Final version

---
+++ README.md
@@ -0,0 +1,5 @@
+# task_list
+
+A new Flutter project.
+
+Projeto apra estudo, lista de tarefas com Flutter.
---
+++ android/app/src/main/kotlin/com/saultary/task_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.saultary.task_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
